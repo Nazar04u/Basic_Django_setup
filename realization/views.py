d3156a4cceb35a58c40f1a17180f56019a7574d1
Nazar04u <nazar.urb@gmail.com>
2024-12-26 10:44:41 +0200

    Add user serializer

--- realization/views.py
+++ realization/views.py
@@ -1,9 +1,17 @@
 from django.contrib.auth.models import User
 from django.shortcuts import render
 from rest_framework.generics import ListAPIView
-from tutorial.quickstart.serializers import UserSerializer
-
+from rest_framework.response import Response
+from .serializers import UserSerializer
 
 class HomeView(ListAPIView):
     serializer_class = UserSerializer
     queryset = User.objects.all()
+
+    # Customization should happen here instead of overriding get()
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        return Response({"result": serializer.data})
+
+
